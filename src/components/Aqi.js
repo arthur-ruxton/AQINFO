aa1f8ad7f1fb3be4c7caa487332be6e6315c8903
eddie ruxton <ae.ruxton@gmail.com>
2021-12-24 16:35:23 +0000

    plotly barchart implemented

--- src/components/Aqi.js
+++ src/components/Aqi.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { useState, useEffect } from 'react' 
 import axios from 'axios'
+import Plot from 'react-plotly.js';
 
 const Aqi = () => {
-  const [ data, setData ] = useState({})
+  const [ aqi, setAqi ] = useState({})
 
   useEffect(() => {
     const getAq = async () => {
       try {
         const response = await axios.get('http://api.openweathermap.org/data/2.5/air_pollution?lat=0&lon=51&appid=1520d83412ce9ad4ab01ca7900640776')
         console.log('response', response.data.list[0].components)
-        setData(response.data.list[0].components)
+        setAqi(response.data.list[0].components)
       } catch (error) {
         console.log(error)
       }
@@ -20,27 +21,16 @@ const Aqi = () => {
 
   return (
     <div>
-      <p>
-        Carbon monoxide = {data.co}
-      </p>
-      <p>
-        Nitrogen monoxide = {data.no}
-      </p>
-      <p>
-        Nitrogen dioxide = {data.no2}
-      </p>
-      <p>
-        Ozone = {data.o3}
-      </p>
-      <p>
-        Sulphur dioxide = {data.so2}
-      </p>
-      <p>
-        Ammonia = {data.nh3}
-      </p>
-      <p>
-        Particulates; PM2.5: {data.pm2_5}, PM10: {data.pm10}
-      </p>
+      <Plot 
+        data={[
+          {
+            x: ['Carbon Monoxide', 'Nitrogen Oxide', 'Nitrogen Dioxide', 'Ozone', 'Sulphur Dioxide', 'Ammonia', 'PM2.5', 'PM10'],
+            y: [aqi.co, aqi.no, aqi.no2, aqi.o3, aqi.so2, aqi.nh3, aqi.pm2_5, aqi.pm10],
+            type: 'bar'
+          }
+        ]}
+        layout={ {title: 'Local air quality', yaxis: {title: 'Pollutant concentration in μg/m3'}} }
+      />
     </div>
   )
 }
